191191412ac1cf1752cef92f589e7a0a4d5f62cc
junam2 <speedlin4584@gmail.com>
2022-10-10 22:44:37 +0900

    chore: test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. do-it-client
+          Edit <code>src/App.js</code> and save to reload. do-it-client with Nginx
         </p>
         <a
           className="App-link"
